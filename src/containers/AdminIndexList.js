dabb2dd0f09a27f326d2d851b314f5531ccaa141
Carlos Anriquez <canriquez@gmail.com>
2020-10-07 16:36:33 -0300

    adds react multi caroucel library to handle card sliding

--- src/containers/AdminIndexList.js
+++ src/containers/AdminIndexList.js
@@ -5,8 +5,30 @@ import ApplicationCard from "../components/ApplicationCard";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import dateFormat from "dateformat";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const AdminIndexList = ({ index_report }) => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   const cardObject = (object) => {
     TimeAgo.addLocale(en);
     const timeAgo = new TimeAgo("en-US");
@@ -26,11 +48,16 @@ const AdminIndexList = ({ index_report }) => {
 
   return (
     <div className={styles.carroucelContainer}>
-      {index_report.map((object, id) => {
-        return (
-          <ApplicationCard key={"child" + id} cardObject={cardObject(object)} />
-        );
-      })}
+      <Carousel responsive={responsive}>
+        {index_report.map((object, id) => {
+          return (
+            <ApplicationCard
+              key={"child" + id}
+              cardObject={cardObject(object)}
+            />
+          );
+        })}
+      </Carousel>
     </div>
   );
 };
