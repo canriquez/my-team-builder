e08baeaba1ab7040723b386ed62c7b2e55b35d8e
Carlos Anriquez <canriquez@gmail.com>
2020-10-07 13:06:13 -0300

    ApplicationCard component and module style

--- src/components/ApplicationCard.js
+++ src/components/ApplicationCard.js
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "../styles/ApplicationCard.module.css";
 
-const ApplicationCard = ({}) => {
-  return <div></div>;
+const ApplicationCard = ({ cardObject }) => {
+  const { avatar, app_name, job_name, app_date, app_age } = cardObject;
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.cardHeading}>
+        <h1>{job_name}</h1>
+      </div>
+      <div className={styles.sectionTitle}>
+        <h2>Applicants General Information</h2>
+      </div>
+      <div className={styles.applicantAvatar}>
+        <img src={avatar} alt="user avatar" />
+        <h2>{app_name}</h2>
+        <h4>applied {app_age}</h4>
+      </div>
+      <div className={styles.applicantInfoWrap}>
+        <div className={styles.applicantData}>
+          <h3>Application Date</h3>
+          <h4>{app_date}</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ApplicationCard;
--- src/containers/AdminIndexList.js
+++ src/containers/AdminIndexList.js
@@ -2,15 +2,37 @@ import React from "react";
 import { connect } from "react-redux";
 import styles from "../styles/AdminIndexList.module.css";
 import ApplicationCard from "../components/ApplicationCard";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+import dateFormat from "dateformat";
 
-const AdminIndexList = ({ admin }) => {
-  return <ApplicationCard />;
+const AdminIndexList = ({ index_report }) => {
+  TimeAgo.addLocale(en);
+  const timeAgo = new TimeAgo("en-US");
+  const application_age = timeAgo.format(
+    new Date(index_report[0].aplication_date),
+    "round"
+  );
+
+  const cardObject = {
+    job_name: index_report[0].job_name,
+    avatar: index_report[0].applicant_avatar,
+    app_name: index_report[0].applicant_name,
+    app_age: application_age,
+    app_date: dateFormat(Date(index_report[0].aplication_date)),
+  };
+
+  return (
+    <div className={styles.carroucelContainer}>
+      <ApplicationCard cardObject={cardObject} />
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => ({
   account: state.account,
   secure: state.secure,
-  admin: state.admin,
+  index_report: state.admin.index_report,
 });
 
 export default connect(mapStateToProps, null)(AdminIndexList);
